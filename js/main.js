4890d0ab5c6df5f134cf1e0a948fbd033dd4f674
Ayan Tanveer <c4011762@hallam.shu.ac.uk>
2024-10-30 15:26:19 +0000

    week 6 lab

---
+++ js/main.js
@@ -0,0 +1,35 @@
+console.info("Hello from the file");
+document.getElementById("myHeading").innerHTML = "Ayan Tanveer";
+document.querySelector("nav ul li").setAttribute("class", "currentPage");
+document.querySelector("nav ul li a")
+        .setAttribute("href", "https://www.google.co.uk")
+let colourButtons = document.querySelectorAll(".colPicker");
+console.dir(colourButtons);
+for (let i = 0; i < colourButtons.length; i++) {
+   colourButtons[i].style.backgroundColor = "lightGrey";
+}
+
+document.getElementById("myTestBtn").addEventListener("click", function () {
+console.info("hi i was clicked");
+});
+
+//colour picker
+let redBtn = document.querySelector(".red");
+redBtn.addEventListener("click", function () {
+   document.body.setAttribute("class", "redBack");
+})
+
+let blueBtn = document.querySelector(".blue");
+blueBtn.addEventListener("click", function () {
+   document.body.setAttribute("class", "blueBack");
+})
+
+let greenBtn = document.querySelector(".green");
+greenBtn.addEventListener("click", function () {
+   document.body.setAttribute("class", "greenBack");
+})
+
+let resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", function () {
+   document.body.removeAttribute("class");
+})
